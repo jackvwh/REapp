b1a8c3702f520655e18973d0b041cc61a38b007d
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2023-11-20 18:50:51 +0100

    css for userprofileheader and userprofiledetails

--- Client/src/components/userProfileDetails.jsx
+++ Client/src/components/userProfileDetails.jsx
@@ -1,5 +1,49 @@
-// UserProfileDetails.jsx
 import React, { useState } from 'react';
+
+const containerStyle = {
+  maxWidth: '600px',
+  margin: 'auto',
+  padding: '20px',
+  border: '1px solid #ccc',
+  borderRadius: '8px',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+};
+
+const h2Style = {
+  color: '#333',
+};
+
+const imgStyle = {
+  display: 'block',
+  margin: '10px 0',
+  width: '100px',
+  height: '100px',
+};
+
+const labelStyle = {
+  display: 'block',
+  marginTop: '10px',
+};
+
+const inputStyle = {
+  width: '100%',
+  padding: '8px',
+  marginTop: '5px',
+  marginBottom: '10px',
+  boxSizing: 'border-box',
+};
+
+const textareaStyle = {
+  width: '100%',
+  padding: '8px',
+  marginTop: '5px',
+  marginBottom: '10px',
+  boxSizing: 'border-box',
+};
+
+const radioStyle = {
+  marginRight: '5px',
+};
 
 function UserProfileDetails() {
   const [userData, setUserData] = useState({
@@ -10,7 +54,7 @@ function UserProfileDetails() {
     interests: '',
     hobbies: '',
     about: '',
-    dependences: '', 
+    dependences: '',
   });
 
   const handleInputChange = (e) => {
@@ -24,88 +68,95 @@ function UserProfileDetails() {
   };
 
   return (
-    <div>
-      <h2>Brugerprofil</h2>
+    <div style={containerStyle}>
+      <h2 style={h2Style}>Brugerprofil</h2>
       <form>
-
-      <label>Billede</label>
+        <label style={labelStyle}>Billede</label>
         <img
           src={userData.image}
           alt="user"
-          style={{ width: '100px', height: '100px' }}
+          style={imgStyle}
         />
 
-        <label>Navn:</label>
+        <label style={labelStyle}>Navn:</label>
         <input
           type="text"
           name="fullName"
           value={userData.fullName}
           onChange={handleInputChange}
+          style={inputStyle}
         />
 
-        <label>Email:</label>
+        <label style={labelStyle}>Email:</label>
         <input
           type="email"
           name="email"
           value={userData.email}
           onChange={handleInputChange}
+          style={inputStyle}
         />
 
-        <label>Alder:</label>
+        <label style={labelStyle}>Alder:</label>
         <input
           type="text"
           name="age"
           value={userData.age}
           onChange={handleInputChange}
+          style={inputStyle}
         />
 
-        <label>Interesser & hobby:</label>
+        <label style={labelStyle}>Interesser & hobby:</label>
         <input
           type="text"
           name="interests"
           value={userData.interests}
           onChange={handleInputChange}
+          style={inputStyle}
         />
 
-
-        <label>Om:</label>
+        <label style={labelStyle}>Om:</label>
         <textarea
           name="about"
           value={userData.about}
           onChange={handleInputChange}
+          style={textareaStyle}
         />
 
-        <label>Afhængighed:</label>
+        <label style={labelStyle}>Afhængighed:</label>
         <input
           type="radio"
           name="afhaengighed"
-          value="Alkohol" 
-          checked={userData.afhaengighed === "Option 1"} 
+          value="Alkohol"
+          checked={userData.afhaengighed === 'Option 1'}
           onChange={handleAfhaengighedChange}
+          style={radioStyle}
         />
 
         <input
           type="radio"
           name="afhaengighed"
-          value="Cigaretter" 
-          checked={userData.afhaengighed === "Option 2"} 
+          value="Cigaretter"
+          checked={userData.afhaengighed === 'Option 2'}
           onChange={handleAfhaengighedChange}
+          style={radioStyle}
         />
 
         <input
-            type="radio"
-            name="afhaengighed"
-            value="kaffein" 
-            checked={userData.afhaengighed === "Option 3"} 
-            onChange={handleAfhaengighedChange}
+          type="radio"
+          name="afhaengighed"
+          value="kaffein"
+          checked={userData.afhaengighed === 'Option 3'}
+          onChange={handleAfhaengighedChange}
+          style={radioStyle}
         />
 
         <input
-            type="radio"
-            name="afhaengighed"
-            value="lim" 
-            checked={userData.afhaengighed === "Option 3"} 
-            onChange={handleAfhaengighedChange}
+          type="radio"
+          name="afhaengighed"
+          value="lim"
+          checked={userData.afhaengighed === 'Option 3'}
+          onChange={handleAfhaengighedChange}
+          style={radioStyle}
         />
       </form>
     </div>
