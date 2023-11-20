5dd3b42b7a4055bf8cd951aed16aa51bbdb6fa81
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-11-20 12:28:05 +0100

    added title and picture

---
+++ Client/src/components/homePageLogo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../stylesheets/homepage.css";
+
+function HomePageLogo() {
+    return (
+        <div>
+            <header>
+                <h1>REapp</h1>
+            </header>
+        </div>
+        )
+    }
+
+export default HomePageLogo;
---
+++ Client/src/pages/homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HomePageLogo from "../components/homePageLogo";
+import HomePagePicture from "../components/homePagePicture";
+
+import "../stylesheets/homepage.css";
+
+function App() {
+  return (
+    <div>
+        <HomePageLogo />
+        <HomePagePicture />
+    </div>
+  );
+}
+
+export default App;
