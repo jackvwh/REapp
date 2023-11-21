9bc015a236a13a22230d72ef0d464c7b733b978e
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2023-11-21 11:06:40 +0100

    modal added

--- Client/src/components/userProfileDetails.jsx
+++ Client/src/components/userProfileDetails.jsx
@@ -1,55 +1,5 @@
 import React, { useState } from 'react';
-
-const containerStyle = {
-  maxWidth: '600px',
-  margin: 'auto',
-  padding: '20px',
-  border: '1px solid #ccc',
-  borderRadius: '8px',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-};
-
-const h2Style = {
-  color: '#333',
-};
-
-const imgStyle = {
-  display: 'block',
-  margin: '10px 0',
-  width: '100px',
-  height: '100px',
-};
-
-const labelStyle = {
-  display: 'block',
-  marginTop: '10px',
-};
-
-const inputStyle = {
-  width: '100%',
-  padding: '8px',
-  marginTop: '5px',
-  marginBottom: '10px',
-  boxSizing: 'border-box',
-};
-
-const textareaStyle = {
-  width: '100%',
-  padding: '8px',
-  marginTop: '5px',
-  marginBottom: '10px',
-  boxSizing: 'border-box',
-};
-
-const radioLabelStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  marginBottom: '5px',
-};
-
-const radioStyle = {
-  marginRight: '5px',
-};
+import '../stylesheets/userProfile.css';
 
 function UserProfileDetails() {
   const [userData, setUserData] = useState({
@@ -62,6 +12,16 @@ function UserProfileDetails() {
     about: '',
     dependences: '',
   });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -86,113 +46,120 @@ function UserProfileDetails() {
   };
 
   return (
-    <div style={containerStyle}>
-      <h2 style={h2Style}>Brugerprofil</h2>
-      <form>
-        <label style={labelStyle}>Billede</label>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          style={inputStyle}
-        />
-        <img src={userData.image} alt="user" style={imgStyle} />
+    <div>
+      <button class = "button" onClick={openModal}>Edit Profile</button>
 
-        <label style={labelStyle}>Navn:</label>
-        <input
-          type="text"
-          name="fullName"
-          value={userData.fullName}
-          onChange={handleInputChange}
-          style={inputStyle}
-        />
+      {isModalOpen && (
+        <div className="modal-container">
+          <div className="modal-content">
+            <div className="containerStyle">
+              <h2 className="h2Style">Brugerprofil</h2>
+              <form>
+                <label className="labelStyle">Billede</label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
+                <img src={userData.image} alt="user" className="imgStyle" />
 
-        <label style={labelStyle}>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={userData.email}
-          onChange={handleInputChange}
-          style={inputStyle}
-        />
+                <label className="labelStyle">Navn:</label>
+                <input
+                  type="text"
+                  name="fullName"
+                  value={userData.fullName}
+                  onChange={handleInputChange}
+                />
 
-        <label style={labelStyle}>Alder:</label>
-        <input
-          type="text"
-          name="age"
-          value={userData.age}
-          onChange={handleInputChange}
-          style={inputStyle}
-        />
+                <label className="labelStyle">Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={userData.email}
+                  onChange={handleInputChange}
+                />
 
-        <label style={labelStyle}>Interesser & hobby:</label>
-        <input
-          type="text"
-          name="interests"
-          value={userData.interests}
-          onChange={handleInputChange}
-          style={inputStyle}
-        />
+                <label className="labelStyle">Alder:</label>
+                <input
+                  type="text"
+                  name="age"
+                  value={userData.age}
+                  onChange={handleInputChange}
+                />
 
-        <label style={labelStyle}>Om:</label>
-        <textarea
-          name="about"
-          value={userData.about}
-          onChange={handleInputChange}
-          style={textareaStyle}
-        />
+                <label className="labelStyle">Interesser & hobby:</label>
+                <input
+                  type="text"
+                  name="interests"
+                  value={userData.interests}
+                  onChange={handleInputChange}
+                />
 
-        <label style={labelStyle}>Afhængighed:</label>
-        <label style={radioLabelStyle}>
-          <input
-            type="radio"
-            name="dependences"
-            value="Alkohol"
-            checked={userData.dependences === 'Alkohol'}
-            onChange={handleDependencesChange}
-            style={radioStyle}
-          />
-          Alkohol
-        </label>
+                <label className="labelStyle">Om:</label>
+                <textarea
+                  name="about"
+                  value={userData.about}
+                  onChange={handleInputChange}
+                  className="textAreaStyle"
+                />
 
-        <label style={radioLabelStyle}>
-          <input
-            type="radio"
-            name="dependences"
-            value="Cigaretter"
-            checked={userData.dependences === 'Cigaretter'}
-            onChange={handleDependencesChange}
-            style={radioStyle}
-          />
-          Cigaretter
-        </label>
+                <label className="labelStyle">Afhængighed:</label>
+                <label className="radioLabelStyle">
+                  <input
+                    type="radio"
+                    name="dependences"
+                    value="Alkohol"
+                    checked={userData.dependences === 'Alkohol'}
+                    onChange={handleDependencesChange}
+                    className="radioStyle"
+                  />
+                  Alkohol
+                </label>
 
-        <label style={radioLabelStyle}>
-          <input
-            type="radio"
-            name="dependences"
-            value="kaffein"
-            checked={userData.dependences === 'kaffein'}
-            onChange={handleDependencesChange}
-            style={radioStyle}
-          />
-          Kaffein
-        </label>
+                <label className="radioLabelStyle">
+                  <input
+                    type="radio"
+                    name="dependences"
+                    value="Cigaretter"
+                    checked={userData.dependences === 'Cigaretter'}
+                    onChange={handleDependencesChange}
+                    className="radioStyle"
+                  />
+                  Cigaretter
+                </label>
 
-        <label style={radioLabelStyle}>
-          <input
-            type="radio"
-            name="dependences"
-            value="lim"
-            checked={userData.dependences === 'lim'}
-            onChange={handleDependencesChange}
-            style={radioStyle}
-          />
-          Lim
-        </label>
-      </form>
+                <label className="radioLabelStyle">
+                  <input
+                    type="radio"
+                    name="dependences"
+                    value="kaffein"
+                    checked={userData.dependences === 'kaffein'}
+                    onChange={handleDependencesChange}
+                    className="radioStyle"
+                  />
+                  Kaffein
+                </label>
+
+                <label className="radioLabelStyle">
+                  <input
+                    type="radio"
+                    name="dependences"
+                    value="lim"
+                    checked={userData.dependences === 'lim'}
+                    onChange={handleDependencesChange}
+                    className="radioStyle"
+                  />
+                  Lim
+                </label>
+              </form>
+              <button class = "button" onClick={closeModal}>Close</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default UserProfileDetails;
+
--- Client/src/components/userProfileHeader.jsx
+++ Client/src/components/userProfileHeader.jsx
@@ -1,41 +1,15 @@
+
 import React from 'react';
-
-const headerStyle = {
-  backgroundColor: '#3498db', 
-  color: '#fff', 
-  padding: '15px',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-};
-
-const h1Style = {
-  margin: '0', 
-};
-
-const buttonContainerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
-const buttonStyle = {
-  margin: '0 10px', 
-  padding: '10px',
-  backgroundColor: '#fff',
-  color: '#3498db', 
-  border: 'none',
-  cursor: 'pointer',
-};
+import '../stylesheets/userProfile.css'; 
 
 function UserProfileHeader() {
   return (
-    <header style={headerStyle}>
-      <h1 style={h1Style}>REapp</h1>
-      <div style={buttonContainerStyle}>
-        <button style={buttonStyle}>Ressoucer</button>
-        <button style={buttonStyle}>Forside</button>
-        <button style={buttonStyle}>Fællesskab</button>
+    <header className="user-profile-header">
+      <h1>REapp</h1>
+      <div className="button-container">
+        <button class = "button">Ressoucer</button>
+        <button class = "button">Forside</button>
+        <button class = "button">Fællesskab</button>
       </div>
     </header>
   );
