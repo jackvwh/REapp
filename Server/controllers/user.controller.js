573bfb3e806d73ee539579446be201ce6053875c
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2023-11-22 10:45:35 +0100

    OOP class for usercontrollers

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,47 +1,50 @@
-import {
+
+  import {
     createUser_db,
     deleteUser_db,
     readUserById_db,
     readAllUsers_db,
     updateUser_db,
   } from '../models/albums.models.js';
-
-
-  export async function updateUser(req, res) {
-    const id = req.params.id;
-    const { image, fullName, email, age, interests, hobbies, about } = req.body;
   
-    try {
-      const updatedUser= await updateUser_db(
-        id,
-        image,
-        fullName,
-        email,
-        age,
-        interests,
-        hobbies,
-        about
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occured while updating user' });
+  class UserController {
+    async updateUser(req, res) {
+      const id = req.params.id;
+      const { image, fullName, email, age, interests, hobbies, about } = req.body;
+  
+      try {
+        const updatedUser = await updateUser_db(
+          id,
+          image,
+          fullName,
+          email,
+          age,
+          interests,
+          hobbies,
+          about
+        );
+        res.status(200).json(updatedUser);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while updating user' });
+      }
     }
-  }
-
-    export function deleteUser(req, res) {
-    const id = req.params.id;
-
-    try {
+  
+    deleteUser(req, res) {
+      const id = req.params.id;
+  
+      try {
         const deletedUser = deleteUser_db(id);
         res.status(200).json(deletedUser);
-        }
-    catch (err) {
+      } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'An error occured while deleting user' });
-        }
+        res.status(500).json({ error: 'An error occurred while deleting user' });
+      }
     }
-
+  }
+  
+  export default new UserController();
+  
 
 
   
