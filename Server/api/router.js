3e1f7d835696a401c4657ec833bb1c1ea332cf6f
jackvwh <jackvwh@hotmail.com>
2023-11-23 08:24:16 +0100

    init routes setup & db config

---
+++ Server/api/router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+export default Router();
+//   .use("/", homeRouter)
+//   .use("/users", userRouter);
+//   .use("/chat", chatRouter);
+//   .use("/login", loginRouter);
+//   .use("/signup", signupRouter);
+//   .use("/calender", calenderRouter);
+//   .use("/survey", surveyRouter);
