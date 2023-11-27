a4cc5dd9ee51d0f53af5d76d844f415d24487449
Sander Johansen <55443844+SanderJohansen@users.noreply.github.com>
2023-11-27 13:47:28 +0100

    begining of routing, + deleting unessecary react stuff
    i have also made sure the server.js works with our new "module" setup instead of commonjs

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,33 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config.js";
-import router from "./api/router.js";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
+dotenv.config();
+//const routes = routes = require("./api/server/router");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
-app.use(cors());
-app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the root of your project
+const projectRoot = path.join(__dirname, '../..');
+
+// Serve static files from the React app
+app.use(express.static(path.join(projectRoot, '/reapp/client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(projectRoot, 'reapp/client/build', 'index.html'));
 });
+
+// Uncomment and modify this part once you have your routes set up
+// import routes from './api/server/router';
+// app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API server running on PORT ${PORT}!`);
+});
+
+
