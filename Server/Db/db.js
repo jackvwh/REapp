f8a22c6fd996aa588efbb0a735d1fdc0e1c12a9a
jackvwh <jackvwh@hotmail.com>
2023-11-27 13:54:07 +0100

    db config fix

--- Server/Db/db.js
+++ Server/Db/db.js
@@ -13,20 +13,18 @@ const dbconfig = {
   // eslint-disable-next-line no-undef
   database: process.env.MYSQL_DATABASE,
   multipleStatements: true,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+  
 };
 
-const mysqlConnection = mysql.createPool(dbconfig).getConnection(err => {
+const mysqlConnection = mysql.createConnection(dbconfig);
+
+mysqlConnection.connect(function(err) {
   if (err) {
-    console.log('Error connecting to database', err);
-  } else {
-    console.log('Connected to database');
+    return console.error('error: ' + err.message);
   }
+
+  console.log('Connected to the MySQL server.');
 });
+
+
 export default mysqlConnection;
