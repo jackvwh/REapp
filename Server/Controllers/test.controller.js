6d087f334d870d210301d11219ce5ac1169b5f54
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2023-12-02 11:23:38 +0100

    test controller updated

--- Server/Controllers/test.controller.js
+++ Server/Controllers/test.controller.js
@@ -1,9 +1,28 @@
 import SurveyModel from '../Models/survey.model.js';
 import QuestionModel from '../Models/question.model.js';
 import FeedbackModels from '../Models/feedback.model.js';
+import ActivityModels from '../Models/activity.model.js';
+import UserModels from '../Models/users.models.js';
+import PrivilegeModels from '../Models/privilige.models.js';
 
 export default class testdataController {
   static async insertTestData(req, res) {
+    priviliges.forEach(async privilige => {
+      await PrivilegeModels.insertPrivilege(privilige);
+    });
+
+    users.forEach(async user => {
+      await UserModels.createUser(
+        user.username,
+        user.password,
+        user.first_name,
+        user.last_name,
+        user.email,
+        user.birthdate,
+        user.privilege
+      );
+    });
+
     questions.forEach(async question => {
       await QuestionModel.createOne(question.question, question.answerType);
     });
@@ -14,16 +33,26 @@ export default class testdataController {
         survey.questions
       );
     });
-    FeedbackModels.createFeedback(1, 1);
-    FeedbackModels.createFeedback(1, 2);
+    FeedbackModels.createFeedback(3, 4);
+    FeedbackModels.createFeedback(4, 3);
 
     await FeedbackModels.insertFeedbackAnswers(1, answers1);
     await FeedbackModels.insertFeedbackAnswers(2, answers2);
 
+    activities.forEach(async activity => {
+      await ActivityModels.insertActivity(
+        activity.activityType,
+        activity.activityDescription
+      );
+    });
+
     res.status(200).json({ message: 'Test data inserted successfully' });
   }
 }
-
+const priviliges = [
+  { title: 'user', privilige: 'read, write' },
+  { title: 'admin', privilige: 'read, write, delete' },
+];
 const questions = [
   {
     question: 'Do you enjoy working in a team environment?',
@@ -170,5 +199,139 @@ const answers2 = [
     answerText: 'no not at all i am weak',
     answerValue: null,
     answerBool: null,
+  },
+];
+const activities = [
+  {
+    activityType: 'Fodbold',
+    activityDescription: 'Tosser der løber efter en bold',
+  },
+  {
+    activityType: 'Basketball',
+    activityDescription: 'Sport med en bold og en kurv',
+  },
+  {
+    activityType: 'Tennis',
+    activityDescription: 'En sport med en bold og en tennisketsjer',
+  },
+  {
+    activityType: 'Hockey',
+    activityDescription: 'Sport med en puck og en isbane',
+  },
+  {
+    activityType: 'Svømning',
+    activityDescription: 'Sport i vandet med forskellige stilarter',
+  },
+  {
+    activityType: 'Baseball',
+    activityDescription: 'Sport med en bold og en baseball-kølle',
+  },
+  {
+    activityType: 'Golf',
+    activityDescription: 'Sport med en bold og en golfkølle',
+  },
+  {
+    activityType: 'Badminton',
+    activityDescription: 'Sport med en fjerbold og ketsjer',
+  },
+  {
+    activityType: 'Volleyball',
+    activityDescription: 'Sport med en bold over et net',
+  },
+  {
+    activityType: 'Atletik',
+    activityDescription: 'Forskellige sportsdiscipliner som løb, spring og kast',
+  },
+];
+const users = [
+  {
+    username: 'user1',
+    password: 12345,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'john.doe@example.com',
+    birthdate: '1990-01-15',
+    privilege: 1,
+  },
+  {
+    username: 'user2',
+    password: 67890,
+    first_name: 'Alice',
+    last_name: 'Smith',
+    email: 'alice.smith@example.com',
+    birthdate: '1985-05-20',
+    privilege: 1,
+  },
+  {
+    username: 'user3',
+    password: 13579,
+    first_name: 'Ella',
+    last_name: 'Johnson',
+    email: 'ella.johnson@example.com',
+    birthdate: '1993-11-30',
+    privilege: 1,
+  },
+  {
+    username: 'user4',
+    password: 24680,
+    first_name: 'Michael',
+    last_name: 'Brown',
+    email: 'michael.brown@example.com',
+    birthdate: '1988-07-12',
+    privilege: 1,
+  },
+  {
+    username: 'user5',
+    password: 98765,
+    first_name: 'Olivia',
+    last_name: 'Wilson',
+    email: 'olivia.wilson@example.com',
+    birthdate: '1996-03-25',
+    privilege: 1,
+  },
+  {
+    username: 'user6',
+    password: 54321,
+    first_name: 'James',
+    last_name: 'Lee',
+    email: 'james.lee@example.com',
+    birthdate: '1982-09-05',
+    privilege: 1,
+  },
+  {
+    username: 'user7',
+    password: 112233,
+    first_name: 'Ava',
+    last_name: 'Anderson',
+    email: 'ava.anderson@example.com',
+    birthdate: '1991-12-08',
+    privilege: 1,
+  },
+  {
+    username: 'user8',
+    password: 445566,
+    first_name: 'William',
+    last_name: 'Garcia',
+    email: 'william.garcia@example.com',
+    birthdate: '1987-04-17',
+    privilege: 1,
+  },
+  {
+    username: 'user9',
+    password: 998877,
+    first_name: 'Sophia',
+    last_name: 'Martinez',
+    email: 'sophia.martinez@example.com',
+    birthdate: '1994-06-03',
+    privilege: 1,
+  },
+  {
+    username: 'user10',
+    password: 556677,
+    first_name: 'Liam',
+    last_name: 'Robinson',
+    email: 'liam.robinson@example.com',
+    birthdate: '1989-10-22',
+    privilege: 1,
   },
 ];
