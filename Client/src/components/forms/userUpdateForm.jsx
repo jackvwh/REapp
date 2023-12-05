fa15da6d24a3a217db9bcdc77dc9d6e85b2ced58
jackvwh <jackvwh@hotmail.com>
2023-12-05 14:02:44 +0100

    userUpdateform updated

--- Client/src/components/forms/userUpdateForm.jsx
+++ Client/src/components/forms/userUpdateForm.jsx
@@ -72,91 +72,89 @@ export default function UserUpdateForm({ userData }) {
     }
   }
   return (
-    <div className="modal-container">
-      <div className="modal-content">
-        <div className="containerStyle">
-          <h2 className="h2Style">Rediger</h2>
-          <form>
-            <label className="labelStyle">Brugernavn:</label>
-            <input
-              type="text"
-              name="username"
-              value={userDetails.username}
-              onChange={handleInputChange}
-            />
+    <div className="modal-box">
+      <h3 className="font-bold text-lg">Opdater bruger</h3>
+      <div className="modal-action">
+        <form method="dialog">
+          <label className="labelStyle">Brugernavn:</label>
+          <input
+            type="text"
+            name="username"
+            value={userDetails.username}
+            onChange={handleInputChange}
+          />
 
-            <label className="labelStyle">Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={userDetails.password}
-              onChange={handleInputChange}
-            />
+          <label className="labelStyle">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={userDetails.password}
+            onChange={handleInputChange}
+          />
 
-            <label className="labelStyle">Førstenavn:</label>
-            <input
-              type="text"
-              name="firstName"
-              value={userDetails.firstName}
-              onChange={handleInputChange}
-            />
+          <label className="labelStyle">Førstenavn:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={userDetails.firstName}
+            onChange={handleInputChange}
+          />
 
-            <label className="labelStyle">Efternavn:</label>
-            <input
-              type="text"
-              name="lastName"
-              value={userDetails.lastName}
-              onChange={handleInputChange}
-            />
+          <label className="labelStyle">Efternavn:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={userDetails.lastName}
+            onChange={handleInputChange}
+          />
 
-            <label className="labelStyle">Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={userDetails.email}
-              onChange={handleInputChange}
-            />
+          <label className="labelStyle">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={userDetails.email}
+            onChange={handleInputChange}
+          />
 
-            <label className="labelStyle">Alder:</label>
-            <input
-              type="date"
-              name="birthdate"
-              onChange={handleInputChange}
-              required
-            />
+          <label className="labelStyle">Alder:</label>
+          <input
+            type="date"
+            name="birthdate"
+            onChange={handleInputChange}
+            required
+          />
 
-            <label className="labelStyle">Interesser:</label>
-            <div className="interests-container">
-              <Select
-                options={
-                  (activityOptions &&
-                    activityOptions.map(activity => ({
-                      value: activity.activity_type,
-                      label: activity.activity_type,
-                    }))) || { value: 'Loading...', label: 'Loading...' }
-                }
-                isMulti
-                onChange={handleActivityChange}
-                placeholder="Vælg interesser"
-                value={
-                  userActivities &&
-                  userActivities.map(activity => ({
-                    value: activity,
-                    label: activity,
-                  }))
-                }
-              />
-            </div>
-            <div className="button-container">
-              <button className="button" onClick={onSubmit} disabled={updating}>
-                {updating ? <Spinner /> : 'Gem'}
-              </button>
-              <button className="button" onClick={closeModal}>
-                Luk
-              </button>
-            </div>
-          </form>
-        </div>
+          <label className="labelStyle">Interesser:</label>
+          <div className="interests-container">
+            <Select
+              options={
+                (activityOptions &&
+                  activityOptions.map(activity => ({
+                    value: activity.activity_type,
+                    label: activity.activity_type,
+                  }))) || { value: 'Loading...', label: 'Loading...' }
+              }
+              isMulti
+              onChange={handleActivityChange}
+              placeholder="Vælg interesser"
+              value={
+                userActivities &&
+                userActivities.map(activity => ({
+                  value: activity,
+                  label: activity,
+                }))
+              }
+            />
+          </div>
+          <button className="button" onClick={onSubmit} disabled={updating}>
+            {updating ? <Spinner /> : 'Gem'}
+          </button>
+          <button
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            onClick={() => document.getElementById('my_modal_4').close()}>
+            X
+          </button>
+        </form>
       </div>
     </div>
   );
--- Client/src/components/userProfileDetails.jsx
+++ Client/src/components/userProfileDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useApiClient } from '../Hooks/useApiClient';
 import UserUpdateForm from './forms/userUpdateForm';
 
@@ -7,12 +7,6 @@ import UserUpdateForm from './forms/userUpdateForm';
 const userId = 5;
 
 function UserProfileDetails() {
-  // modal
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
   // get user from custom useEffect hook
   const {
     data: userData,
@@ -27,7 +21,9 @@ function UserProfileDetails() {
 
   return (
     <div>
-      {isModalOpen && userData && <UserUpdateForm userData={userData} />}
+      <dialog id="my_modal_4" className="modal">
+        <UserUpdateForm userData={userData} />
+      </dialog>
 
       <div className="display-mode">
         <h2 className="h2Style">Brugerprofil</h2>
@@ -41,41 +37,37 @@ function UserProfileDetails() {
             </div>
 
             <div className="item2 itemStyle">
-              <p className="item-title">Password:</p>
-              <p>{userData.password}</p>
-            </div>
-
-            <div className="item3 itemStyle">
               <p className="item-title">Førstenavn:</p>
               <p>{userData.firstName}</p>
             </div>
-            <div className="item4 itemStyle">
+            <div className="item3 itemStyle">
               <p className="item-title">Efternavn:</p>
               <p>{userData.lastName}</p>
             </div>
-            <div className="item5 itemStyle">
+            <div className="item4 itemStyle">
               <p className="item-title">Email:</p>
               <p>{userData.email}</p>
             </div>
-            <div className="item6 itemStyle">
+            <div className="item5 itemStyle">
               <p className="item-title">Alder:</p>
               <p>{calcAge(userData.birthdate)}</p>
             </div>
-            <div className="item7 itemStyle">
+            <div className="item6 itemStyle">
               <p className="item-title">Aktiviteter:</p>
               {userData &&
                 userData.activities.map((activity, index) => (
                   <span
                     key={activity.activityType || index}
-                    className="badge badge-lg"
-                  >
+                    className="badge badge-lg">
                     {activity.activityType}
                   </span>
                 ))}
             </div>
             <div>
-              <button className="button" onClick={openModal}>
-                Opdater profil
+              <button
+                className="button"
+                onClick={() => document.getElementById('my_modal_4').showModal()}>
+                Opdater oplysninger
               </button>
             </div>
           </div>
