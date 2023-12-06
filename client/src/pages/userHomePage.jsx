b14d524bbca3d011cc320dba10ba8345e376d2ef
Sander Johansen <55443844+SanderJohansen@users.noreply.github.com>
2023-12-06 12:40:11 +0100

    calendre test ui med MOBISCROLL

---
+++ client/src/pages/userHomePage.jsx
@@ -0,0 +1,15 @@
+// UserHomePage.jsx
+import React from 'react';
+import Calender from '../components/calender/calender';
+
+
+function UserProfile() {
+  return (
+    <div>
+      <Calender />
+
+    </div>
+  );
+}
+
+export default UserProfile;
