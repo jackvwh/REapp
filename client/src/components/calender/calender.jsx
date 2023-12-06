ed111e2bd02db21a15476550953d9f02fdd52c43
Sander Johansen <55443844+SanderJohansen@users.noreply.github.com>
2023-12-06 13:35:55 +0100

    calender works, now we add the rest

--- client/src/components/calender/calender.jsx
+++ client/src/components/calender/calender.jsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+import  '../../styles/calender.css';
 
+const Calendar = () => {
+  const calendarRef = useRef();
+  const [config, setConfig] = useState({
+    locale: "da-dk",
+    viewType: "Week",
+    headerDateFormat: "d MMMM yyyy",
+    timeRangeSelectedHandling: "Enabled",
+    onTimeRangeSelected: async (args) => {
+      const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+      const dp = args.control;
+      dp.clearSelection();
+      if (modal.canceled) { return; }
+      dp.events.add({
+        start: args.start,
+        end: args.end,
+        id: DayPilot.guid(),
+        text: modal.result
+      });
+    },
+    eventMoveHandling: "Update",
+    onEventMoved: (args) => {
+      console.log("Event moved: " + args.e.text());
+    },
+    eventResizeHandling: "Update",
+    onEventResized: (args) => {
+      console.log("Event resized: " + args.e.text());
+    },
+    eventClickHandling: "Disabled",
+  });
 
-const calender = () => {
+  useEffect(() => {
 
+    const events = [
+      {
+        id: 1,
+        text: "Event 1",
+        start: DayPilot.Date.today().addHours(10),
+        end: DayPilot.Date.today().addHours(12),
+        resource: "R1"
+      },
+      {
+        id: 2,
+        text: "Event 2",
+        start: "2024-06-02T10:00:00",
+        end: "2024-06-02T11:00:00",
+        resource: "R2",
+        barColor: "#38761d",
+        barBackColor: "#93c47d"
+      }
+    ];
 
-export default calender;
+    // load event data using config
+    setConfig(prevConfig => ({
+      ...prevConfig,
+      events
+    }));
+
+    // to improve performance, you can use the direct API to load resources and events instead:
+    // getCalendar().update({events, columns});
+
+  }, []);
+
+  const getCalendar = () => calendarRef.current?.control;
+
+  return (
+    <div className='calendar-styling'>
+      <DayPilotCalendar
+        {...config}
+        ref={calendarRef}
+      />
+    </div>
+  );
+}
+export default Calendar;
--- client/src/pages/userHomePage.jsx
+++ client/src/pages/userHomePage.jsx
@@ -1,12 +1,15 @@
 // UserHomePage.jsx
 import React from 'react';
 import Calender from '../components/calender/calender';
+import UserProfileHeader from '../components/userProfileHeader.jsx';
 
 
 function UserProfile() {
   return (
     <div>
+      <UserProfileHeader />
       <Calender />
+      
 
     </div>
   );
