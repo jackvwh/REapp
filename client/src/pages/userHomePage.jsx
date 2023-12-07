cb72dcf972cba88863e4f64d8f0af5aef0c429e4
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-12-07 11:33:36 +0100

    added notificationMeassage component

--- client/src/pages/userHomePage.jsx
+++ client/src/pages/userHomePage.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Calender from '../components/calender/calender';
 import UserProfileHeader from '../components/userProfileHeader.jsx';
+import NotificationMessage from '../components/notifications/notificationMessage.jsx';
 
 //Might add a "Goddag, <user>" here later on
 
@@ -14,13 +15,33 @@ function UserProfile() {
           <h1 className="bg-primary text-white text-xl p-4 rounded-lg">
             Notifikationer
           </h1>
-          <section>
-            <h2>Placeholder</h2>
-            <p>Placeholder</p>
-          </section>
+          <div>
+            <NotificationMessage />
+          </div>
         </section>
         <Calender />
-        <footer className="footer-styling">Placeholder</footer>
+        <footer className="footer footer-styling">
+          <nav>
+            <header className="footer-title">Services</header>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Company</header>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Legal</header>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
       </div>
     </div>
   );
